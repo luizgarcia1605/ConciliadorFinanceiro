2d8034db4abe293a3f6abaf1caaf6b8062ce9d19
Luiz Rodrigues <luizgarcia_1605@hotmail.com>
2020-03-29 22:05:39 -0300

    Ajuste do campo de pesquisa

--- ConciliadorFinanceiro.Web/wwwroot/js/site.js
+++ ConciliadorFinanceiro.Web/wwwroot/js/site.js
@@ -1,18 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-$.validator.methods.range = function (value, element, param) {
+﻿$.validator.methods.range = function (value, element, param) {
     var globalizedValue = value.replace(",", ".");
     return this.optional(element) || (globalizedValue >= param[0] && globalizedValue <= param[1]);
 }
-
 $.validator.methods.number = function (value, element) {
     return this.optional(element) || /-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
 }
-//Date dd/MM/yyyy
 $.validator.methods.date = function (value, element) {
     var date = value.split("/");
     return this.optional(element) || !/Invalid|NaN/.test(new Date(date[2], date[1], date[0]).toString());
 }
+
+function PesquisarLancamentoPorId() {
+
+    var idtxt = document.getElementById('txtPesquisar').value;
+
+    debugger;
+    $.ajax({
+        url: "/LancamentoFinanceiro/Detalhar/" + idtxt,
+        type: "GET",
+        dataType: 'html',
+        success: function () { },
+        error: function (xhr) {
+            console.log(xhr);
+        }
+    });
+
+    window.location.href = "/LancamentoFinanceiro/Detalhar/" + document.getElementById('txtPesquisar').value;
+};
